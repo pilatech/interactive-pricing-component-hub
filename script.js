cd4b1e3274c42f5685c98c39234190f477ed6915
pilate <pilatech6@gmail.com>
2022-08-07 11:46:00 +0200

    initial setup

---
+++ script.js
@@ -0,0 +1,24 @@
+const packages = [
+ {
+  package: '10k',
+  price: 8
+ },
+ {
+  package: '50k',
+  price: 12
+ },
+ {
+  package: '100k',
+  price: 16
+ },
+ {
+  package: '500k',
+  price: 24
+ },
+ {
+  package: '1m',
+  price: 36
+ }
+]
+
+let billingCycle = 'monthly'
